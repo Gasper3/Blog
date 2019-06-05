7e4121746a9c77806951e67b8f4ef5f3dd82b300
Kacper <33937254+Gasper3@users.noreply.github.com>
2019-06-05 09:30:14 +0200

    Pattern for add.php. There will be visual changes as well

--- admin/add.php
+++ admin/add.php
@@ -10,8 +10,38 @@
 <head>
   <title>Dodaj wpis</title>
   <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
+  <div class="container">
+    <header>
+      <a href="admin.php" class=admin>Admin</a>
+      <a href="../logout.php" class="admin"><img src="../img/logout.png" class="icon"></a>
+    </header>
+    <ul>
+      <li><span class="nav">NAWIGACJA</span></li>
+      <li><a href="">Wyświetl wszystkie</a></li>
+      <li><a href="add.php" style="color: white;">Dodaj wpis</a></li>
+      <li><a href="">Edytuj wpis</a></li>
+      <li><a href="">Usuń wpis</a></li>
+      <li><span class="nav">SZUKAJ</span></li>
+      <li><input type="text" class="search"></li>
+    </ul>
+
+    <footer>
+      <h5>Made by Gasper3<br />
+    </footer>
+  </div>
+  <div class="main">
+    <form method="post">
+      <h2>Nagłówek</h2>
+      <p><input type="text" name='headline'></p>
+      <h2>Zawartość wpisu</h2>
+      <p><textarea name="content"></textarea></p>
+    </form>
+
+    <span class="copyright">Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></span>
+  </div>
 
 
 </body>
--- admin/admin.php
+++ admin/admin.php
@@ -9,23 +9,27 @@ if(!isset($_SESSION['logged'])) {
 <head>
    <title>Panel administracyjny</title>
    <meta charset="utf-8">
-   <link rel="stylesheet" href="style_admin.css">
+   <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div class="container">
     <header>
-      <span class="admin">Admin</span>
+      <a href="admin.php" class=admin>Admin</a>
       <a href="../logout.php" class="admin"><img src="../img/logout.png" class="icon"></a>
     </header>
     <ul>
       <li><span class="nav">NAWIGACJA</span></li>
       <li><a href="">Wyświetl wszystkie</a></li>
-      <li><a href="">Dodaj wpis</a></li>
+      <li><a href="add.php">Dodaj wpis</a></li>
       <li><a href="">Edytuj wpis</a></li>
       <li><a href="">Usuń wpis</a></li>
       <li><span class="nav">SZUKAJ</span></li>
       <li><input type="text" class="search"></li>
     </ul>
+
+    <footer>
+      <h5>Made by Gasper3<br />
+    </footer>
   </div>
   <div class="main">
     <span class="copyright">Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></span>
