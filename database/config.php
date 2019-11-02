880aa75bee65c9ecdfcbd221f19e4992c4c306b2
Gasper3 <trzecik65@gmail.com>
2019-11-02 18:18:41 +0100

    change of login and pass

--- database/config.php
+++ database/config.php
@@ -1,9 +1,9 @@
 <?php
 return [
   "host" => "localhost",
-  "user" => "root",
-  "password" => "",
-  "database" => "blog"
+  "user" => "kacper",
+  "password" => "qwerty",
+  "database" => "blog2"
 ];
 
 
